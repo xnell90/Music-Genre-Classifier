cba6bc90fbfa4c133bac3510ee945a9a655aacea
Alwyn Lopez <alwyn333@hotmail.com>
2021-01-23 21:24:15 -0500

    Added python script file to collect my music collection metadata.

---
+++ data/scripts/extract_metadata.py
@@ -0,0 +1,37 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+tree = ET.parse('my_music_library.xml')
+root = tree.getroot()
+
+main_dict = root.findall('dict')
+
+for item in list(main_dict[0]):
+    if item.tag == "dict":
+        tracks_dict = item
+        break
+
+tracklist = list(tracks_dict.findall('dict'))
+
+music_data = {
+    "Track ID": [],
+    "Name": [],
+    "Artist": [],
+    "Album": [],
+    "Genre": [],
+    "Location": [],
+    "Kind": [],
+    "Size": [],
+    "Total Time": []
+}
+
+for track in tracklist:
+    track_info = list(track)
+    for i in range(len(track_info)):
+        if track_info[i].text in music_data.keys():
+            name = track_info[i].text
+            value = track_info[i + 1].text
+            music_data[name].append(value)
+
+music_df = pd.DataFrame.from_dict(music_data)
+music_df.to_csv("my_music_metadata.csv")
