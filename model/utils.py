95a362af75052bec92cea60b3e135be29d15f10a
Alwyn Lopez <alwyn333@hotmail.com>
2021-04-29 19:36:28 -0400

    Updated README.md and commented out unnecessary code in utils.py

--- model/utils.py
+++ model/utils.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 
-def split_model():
-    file_location = input("Enter File Model Location: ")
-    model = tf.keras.models.load_model(file_location)
+# Added for reference...
+# ----------------------------------------------------
+# file_location = input("Enter File Model Location: ")
+# model = tf.keras.models.load_model(file_location)
 
-    model_architecture = model.to_json()
-    with open("model/model_architecture.json", 'w') as json_file:
-        json_file.write(model_architecture)
-
-    model.save_weights("model/model_weights.h5")
+# model_architecture = model.to_json()
+# with open("model/model_architecture.json", 'w') as json_file:
+#     json_file.write(model_architecture)
+#     model.save_weights("model/model_weights.h5")
+#-----------------------------------------------------
 
 def load_model():
     with open('model/model_architecture.json', 'r') as json_file:
