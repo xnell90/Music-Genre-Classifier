d94838cdef65f6d8284f570c23c8e25e3f98f6a2
Alwyn Lopez <alwyn333@hotmail.com>
2021-05-01 19:56:35 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-#Coming Soon ...
+import pandas as pd
+from model.utils import *
+
+print("-------------------------------------")
+file_location = input("Enter File Location: ")
+genre, probabilities = predict_genre(file_location)
+table = pd.DataFrame(
+    {
+        "Genre": ["Hip-Hop", "Jazz", "Pop", "Rock", "Soundtrack"],
+        "Probability": list(probabilities.round(2))
+    }
+)
+print("Predicted Genre: ", genre)
+print("-------------------------------------")
+print(table)
+print("-------------------------------------")
