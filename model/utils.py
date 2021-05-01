10e77b32625599a5bda01f1fe27a6aa596dd0da3
Alwyn Lopez <alwyn333@hotmail.com>
2021-04-30 22:33:12 -0400

    Added new functions in utils.py

--- model/utils.py
+++ model/utils.py
@@ -1,4 +1,10 @@
+import numpy as np
 import tensorflow as tf
+
+from librosa import load, power_to_db, zero_crossings
+from librosa.beat import tempo
+from librosa.feature import melspectrogram, spectral_flatness
+from librosa.onset import onset_strength
 
 # Added for reference...
 # ----------------------------------------------------
@@ -11,6 +17,14 @@ import tensorflow as tf
 #     model.save_weights("model/model_weights.h5")
 #-----------------------------------------------------
 
+GENRES = {
+    0: 'hip-hop',
+    1: 'jazz',
+    2: 'pop',
+    3: 'rock',
+    4: 'soundtrack'
+}
+
 def load_model():
     with open('model/model_architecture.json', 'r') as json_file:
         model = tf.keras.models.model_from_json(json_file.read())
@@ -18,17 +32,31 @@ def load_model():
 
         return model
 
-def compute_melspectrogram():
-    pass
+def compute_melspectrogram(y, sr):
+    return power_to_db(melspectrogram(y=y, sr=sr), ref=np.max)
 
-def compute_tempo():
-    pass
+def compute_tempo(y, sr):
+    return float(tempo(onset_envelope=onset_strength(y, sr=sr), sr=sr))
 
-def compute_zero_crossing_rate():
-    pass
+def compute_zero_crossing_rate(y):
+    return sum(zero_crossings(y)) / len(zero_crossings(y))
 
-def compute_average_spectral_flatness():
-    pass
+def compute_average_spectral_flatness(y):
+    return np.mean(spectral_flatness(y=y))
 
-def predict_genre():
-    pass
+def predict_genre(file_location):
+    y, sr = load(file_location)
+    model = load_model()
+
+    melspectrogram = compute_melspectrogram(y, sr)
+    extracted_features = np.array(
+        [
+            [compute_tempo(y, sr)],
+            [compute_zero_crossing_rate(y)],
+            [compute_average_spectral_flatness(y)],
+        ]
+    )
+    probabilities = model.predict([melspectrogram, extracted_features])
+    index = int(probabilities.argmax(axis=1))
+
+    return GENRES[index]
