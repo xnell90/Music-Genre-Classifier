5892a2d8dd8fd801db095b82c9d21c67bf6348f6
Alwyn Lopez <alwyn333@hotmail.com>
2021-08-15 20:08:35 -0400

    Added dashes

--- model/utils.py
+++ model/utils.py
@@ -12,7 +12,7 @@ from random import randrange
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Added for reference...
-# ----------------------------------------------------
+# ------------------------------------------------------
 # file_location = input("Enter File Model Location: ")
 # model = tf.keras.models.load_model(file_location)
 
@@ -20,7 +20,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # with open("model/model_architecture.json", 'w') as json_file:
 #     json_file.write(model_architecture)
 #     model.save_weights("model/model_weights.h5")
-#-----------------------------------------------------
+#-------------------------------------------------------
 
 def load_model():
     with open('model/model_architecture.json', 'r') as json_file:
